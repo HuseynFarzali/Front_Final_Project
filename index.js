ec560f08de1959a8024b0e2720b7711a05d178fc
HuseynFarzali <huseynyf@code.edu.az>
2023-09-30 17:36:43 +0400

    Masonry and Countdown sections added

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+// <====================== Carousel JS ===========================>
+$('.carousel').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    isFinite: true,
+    autoplaySpeed: 10000,
+    dots: true,
+});
